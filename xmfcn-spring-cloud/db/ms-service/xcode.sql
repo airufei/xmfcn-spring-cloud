24c15684a2d5b4bc8c9204adebfd4fc83224bba4
airufei <199199688@qq.com>
2018-12-11 11:06:43 +0800

    sql提交

--- xmfcn-spring-cloud/db/ms-service/xcode.sql
+++ xmfcn-spring-cloud/db/ms-service/xcode.sql
@@ -26,5 +26,18 @@ CREATE TABLE `t_base_user` (
 -- ----------------------------
 INSERT INTO `t_base_user` VALUES ('1', 'admin', 'e99a18c428cb38d5f260853678922e03', '18', '199199688@qq.com', '', null, null, '', '2017-03-13 16:07:38', '2017-11-08 16:09:38', '0', '', '1');
 
+CREATE TABLE `t_sys_dict` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `DICT_KEY` varchar(200) DEFAULT NULL COMMENT '值',
+  `DICT_VALUE` text COMMENT '名称',
+  `FID` bigint(20) DEFAULT NULL COMMENT '父级ID',
+  `TYPE` varchar(40) DEFAULT NULL COMMENT '类型',
+  `FLAG` int(11) DEFAULT '1' COMMENT '删除标记 -1删除 1正常',
+  `REMARK` text COMMENT '备注',
+  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `UPDATE_TIME` datetime DEFAULT NULL COMMENT '修改时间',
+  `SORT` int(10) DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=499 DEFAULT CHARSET=utf8 COMMENT='字典数据表';
 
 
