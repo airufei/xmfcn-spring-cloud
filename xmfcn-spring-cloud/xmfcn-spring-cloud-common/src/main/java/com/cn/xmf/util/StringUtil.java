7584a7266c37fe0126fb17ad7ed91cb5b04b4ab7
airufei <199199688@qq.com>
2019-01-22 14:20:48 +0800

    代码优化

--- xmfcn-spring-cloud/xmfcn-spring-cloud-common/src/main/java/com/cn/xmf/util/StringUtil.java
+++ xmfcn-spring-cloud/xmfcn-spring-cloud-common/src/main/java/com/cn/xmf/util/StringUtil.java
@@ -776,6 +776,35 @@ public class StringUtil extends StringUtils {
         return ret;
     }
 
+    /**
+     * 获取访问者IP
+     * <p>
+     * 在一般情况下使用Request.getRemoteAddr()即可，但是经过nginx等反向代理软件后，这个方法会失效。
+     * <p>
+     * 本方法先从Header中获取X-Real-IP，如果不存在再从X-Forwarded-For获得第一个IP(用,分割)，
+     * 如果还不存在则调用Request .getRemoteAddr()。
+     *
+     * @param request
+     * @return
+     */
+    public String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Real-IP");
+        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+        ip = request.getHeader("X-Forwarded-For");
+        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个IP值，第一个为真实IP。
+            int index = ip.indexOf(',');
+            if (index != -1) {
+                return ip.substring(0, index);
+            } else {
+                return ip;
+            }
+        } else {
+            return request.getRemoteAddr();
+        }
+    }
     public static void main(String[] args) {
         String password = StringUtil.getEncryptPassword("abc123");
         System.out.println(password);
--- xmfcn-spring-cloud/xmfcn-spring-cloud-zuul/src/main/java/com/cn/xmf/zuul/filter/TokenFilter.java
+++ xmfcn-spring-cloud/xmfcn-spring-cloud-zuul/src/main/java/com/cn/xmf/zuul/filter/TokenFilter.java
@@ -57,65 +57,7 @@ public class TokenFilter extends ZuulFilter {
      */
     @Override
     public boolean shouldFilter() {
-       /* RequestContext requestContext = RequestContext.getCurrentContext();
-        HttpServletRequest request = requestContext.getRequest();
-        logger.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
-        String ipAddr = getIpAddr(request);
-        String ua = request.getHeader("User-Agent");
-        logger.info("=================>请求IP:" + ipAddr + " =========>浏览器信息" + ua);
-        // 记录入参
-        Enumeration<String> enu = request.getParameterNames();
-        String requestURI = request.getRequestURI();
-        while (enu.hasMoreElements()) {
-            String paraName = enu.nextElement();
-            logger.info("In parameter: " + requestURI + "  " + paraName + ": " + request.getParameter(paraName));
-        }
-        String sessionId = request.getRequestedSessionId();
-        if (StringUtil.isBlank(sessionId)) {
-            request.getSession(true);
-        }
-        boolean shouldFilter = true;//需要拦截
-        // ios导航版静态资源过滤
-        if (requestURI.contains("/pc/") || requestURI.contains("/daohangImage/")||requestURI.contains("/api/channel/")) {
-            shouldFilter = false;//不拦截
-            return shouldFilter;
-        }
-        String dictValue =null;// dictService.getDictValue(ConstantUtil.DICT_TYPE_ALLOW_URL, requestURI);
-        if (StringUtil.isNotBlank(dictValue)) {
-            shouldFilter = false;
-        }*/
         return false;
-    }
-
-
-    /**
-     * 获取访问者IP
-     * <p>
-     * 在一般情况下使用Request.getRemoteAddr()即可，但是经过nginx等反向代理软件后，这个方法会失效。
-     * <p>
-     * 本方法先从Header中获取X-Real-IP，如果不存在再从X-Forwarded-For获得第一个IP(用,分割)，
-     * 如果还不存在则调用Request .getRemoteAddr()。
-     *
-     * @param request
-     * @return
-     */
-    private String getIpAddr(HttpServletRequest request) {
-        String ip = request.getHeader("X-Real-IP");
-        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
-            return ip;
-        }
-        ip = request.getHeader("X-Forwarded-For");
-        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
-            // 多次反向代理后会有多个IP值，第一个为真实IP。
-            int index = ip.indexOf(',');
-            if (index != -1) {
-                return ip.substring(0, index);
-            } else {
-                return ip;
-            }
-        } else {
-            return request.getRemoteAddr();
-        }
     }
 
     /**
@@ -128,23 +70,6 @@ public class TokenFilter extends ZuulFilter {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
         HttpServletResponse response = requestContext.getResponse();
-        Object accessToken = request.getParameter("token");
-        // 根据token信息进行相关校验
-        logger.info("accessToken: " + accessToken);
-        boolean checkToken = checkToken(request, response);
-        // token校验不通过，不放行。
-        if (!checkToken) {
-            requestContext.setSendZuulResponse(false);// 对该请求不路由
-            requestContext.set("isSuccess", false);// 设值，让下一个Filter看到上一个Filter的状态
-            // 构建返回信息
-            RetData dataReturn = new RetData();
-            dataReturn.setCode(RetCode.NO_LOGIN);
-            dataReturn.setMessage("登录信息已失效，请重新登录");
-            String jsonString = JSON.toJSONString(dataReturn, SerializerFeature.WriteMapNullValue);
-            requestContext.setResponseBody(jsonString);
-            requestContext.addZuulResponseHeader("content-type", MediaType.APPLICATION_JSON_UTF8_VALUE);
-            return response;
-        }
         requestContext.set("isSuccess", true);// 设值，让下一个Filter看到上一个Filter的状态
         return null;
     }
