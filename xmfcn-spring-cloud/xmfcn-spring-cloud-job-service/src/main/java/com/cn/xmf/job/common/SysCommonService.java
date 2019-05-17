eaf46abbb1a410d94b4d2e8f3f05bd546453d70f
199199688@qq.com <fei640816>
2019-05-18 00:37:59 +0800

    统一命名方式，原项目job-handle 改为job-service

---
+++ xmfcn-spring-cloud/xmfcn-spring-cloud-job-service/src/main/java/com/cn/xmf/job/common/SysCommonService.java
@@ -0,0 +1,225 @@
+package com.cn.xmf.job.common;
+
+import com.alibaba.fastjson.JSONObject;
+import com.cn.xmf.enums.DingMessageType;
+import com.cn.xmf.job.sys.RedisService;
+import com.cn.xmf.model.ding.DingMessage;
+import com.cn.xmf.util.StringUtil;
+import com.cn.xmf.job.sys.DingTalkService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author rufei.cn
+ * <p>公共处理方法模块 $DESCRIPTION</p>
+ */
+@Service
+@SuppressWarnings("all")
+public class SysCommonService {
+
+    private static Logger logger = LoggerFactory.getLogger(SysCommonService.class);
+    @Autowired
+    private DingTalkService dingTalkService;
+    @Autowired
+    private RedisService redisService;
+    @Autowired
+    private Environment environment;
+
+    /**
+     * 获取当前运行的系统名称
+     *
+     * @return
+     */
+    public String getSysName() {
+        return environment.getProperty("spring.application.name");
+    }
+
+    /**
+     * setDingMessage(组织钉钉消息)
+     *
+     * @param method
+     * @param parms
+     * @return
+     */
+    public void sendDingMessage(String method, String parms, String retData, String msg, Class t) {
+        try {
+            DingMessage dingMessage = new DingMessage();
+            dingMessage.setDingMessageType(DingMessageType.MARKDWON);
+            dingMessage.setSysName(getSysName());
+            dingMessage.setModuleName(t.getPackage().toString());
+            dingMessage.setMethodName(method);
+            dingMessage.setParms(parms);
+            dingMessage.setExceptionMessage(msg);
+            dingMessage.setRetData(retData);
+            dingTalkService.sendMessageToDingTalk(dingMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * save(保持缓存)
+     *
+     * @param key
+     * @return
+     */
+    public void save(String key, String value, int seconds) {
+        try {
+            if (StringUtil.isBlank(key)) {
+                return;
+            }
+            redisService.save(key, value, seconds);
+        } catch (Exception e) {
+            logger.error("save_error:" + StringUtil.getExceptionMsg(e));
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * getCache(获取缓存)
+     *
+     * @param key
+     * @return
+     */
+    public String getCache(String key) {
+        String cache = null;
+        if (StringUtil.isBlank(key)) {
+            return null;
+        }
+        try {
+            redisService.getCache(key);
+        } catch (Exception e) {
+            logger.error("getCache_error:" + StringUtil.getExceptionMsg(e));
+            e.printStackTrace();
+        }
+        return cache;
+    }
+
+    /**
+     * delete(删除缓存)
+     *
+     * @param key
+     * @return
+     */
+    public long  delete(String key) {
+        long result=-1;
+        try {
+            if (StringUtil.isBlank(key)) {
+                return result;
+            }
+            result=redisService.delete(key);
+        } catch (Exception e) {
+            logger.error("delete_error:" + StringUtil.getExceptionMsg(e));
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * putToQueue(入队列)
+     *
+     * @param key
+     * @return
+     */
+    public void putToQueue(String key, String value) {
+        try {
+            if (StringUtil.isBlank(key)) {
+                return;
+            }
+            if (StringUtil.isBlank(value)) {
+                return;
+            }
+            redisService.putToQueue(key, value);
+        } catch (Exception e) {
+            logger.error("putToQueue_error:" + StringUtil.getExceptionMsg(e));
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * getFromQueue(获取队列)
+     *
+     * @param key
+     * @return
+     */
+    public String getFromQueue(String key) {
+        String value = null;
+        try {
+            if (StringUtil.isBlank(key)) {
+                return value;
+            }
+            value = redisService.getFromQueue(key);
+        } catch (Exception e) {
+            logger.error("putToQueue_error:" + StringUtil.getExceptionMsg(e));
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    /**
+     * getLock（获取分布式锁）
+     *
+     * @param key
+     * @return
+     * @author airuei
+     */
+    public long getLock(String key) {
+        long lock = -1;
+        if (StringUtil.isBlank(key)) {
+            return lock;
+        }
+        try {
+            Long aLong = redisService.getLock(key);
+            if (aLong != null) {
+                lock = aLong;
+            }
+        } catch (Exception e) {
+            logger.error("getLock（获取分布式锁）:" + StringUtil.getExceptionMsg(e));
+            e.printStackTrace();
+        }
+        return lock;
+    }
+
+    /**
+     * getQueueLength（获取队列长度)key 是消息频道
+     * @param key
+     * @return
+     */
+    public long getQueueLength(String key) {
+        long lock = -1;
+        if (StringUtil.isBlank(key)) {
+            return lock;
+        }
+        try {
+            Long aLong = redisService.getQueueLength(key);
+            if (aLong != null) {
+                lock = aLong;
+            }
+        } catch (Exception e) {
+            logger.error("getQueueLength（获取队列长度):" + StringUtil.getExceptionMsg(e));
+            e.printStackTrace();
+        }
+        return lock;
+    }
+
+    /**
+     * getRedisInfo（redis 运行健康信息)
+     *
+     * @param key
+     * @return
+     */
+    public JSONObject getRedisInfo() {
+        JSONObject result = null;
+        try {
+            result = redisService.getRedisInfo();
+        } catch (Exception e) {
+            logger.error("getRedisInfo（redis 运行健康信息):" + StringUtil.getExceptionMsg(e));
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
---
+++ xmfcn-spring-cloud/xmfcn-spring-cloud-job-service/src/main/java/com/cn/xmf/job/task/RpopQueueTask.java
@@ -0,0 +1,72 @@
+package com.cn.xmf.job.task;
+
+import com.cn.xmf.job.common.SysCommonService;
+import com.cn.xmf.job.core.biz.model.ReturnT;
+import com.cn.xmf.job.core.handler.IJobHandler;
+import com.cn.xmf.job.core.handler.annotation.JobHandler;
+import com.cn.xmf.job.core.log.XxlJobLogger;
+import com.cn.xmf.util.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+/**
+ * 任务Handler示例（Bean模式）
+ * <p>
+ * 开发步骤：
+ * 1、继承"IJobHandler"：“com.cn.xmf.job.core.handler.IJobHandler”；
+ * 2、注册到Spring容器：添加“@Component”注解，被Spring容器扫描为Bean实例；
+ * 3、注册到执行器工厂：添加“@JobHandler(value="自定义jobhandler名称")”注解，注解value值对应的是调度中心新建任务的JobHandler属性的值。
+ * 4、执行日志：需要通过 "XxlJobLogger.user" 打印执行日志；
+ *
+ * @author xuxueli 2015-12-19 19:43:36
+ */
+@JobHandler(value = "rPopQueueTask")
+@Component
+public class RpopQueueTask extends IJobHandler {
+    private static Logger logger = LoggerFactory.getLogger(RpopQueueTask.class);
+
+    @Autowired
+    private SysCommonService sysCommonService;
+
+    @Override
+    public ReturnT<String> execute(String param) throws Exception {
+        XxlJobLogger.log("XMF-JOB, 开始执行");
+        logger.info("开始执行任务");
+        String key = "test_queue_key";
+        long lock = sysCommonService.getLock("queue_" + key);
+        if(lock!=1)
+        {
+            logger.info("正在执行数据，请稍后");
+            return ReturnT.SUCCESS;
+        }
+        long queueLength = sysCommonService.getQueueLength(key);
+        int threadNum = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
+        int perSize = (int) (queueLength / threadNum);
+        for (int i = 0; i < threadNum; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < perSize; j++) {
+                        logger.info("获取队列开始：" + key);
+                        String queue = sysCommonService.getFromQueue(key);
+                        if (StringUtil.isBlank(queue)) {
+                            return;
+                        }
+                        logger.info("获取队列数据：" + queue);
+                    }
+                }
+            });
+        }
+        sysCommonService.delete("queue_"+key);
+        executorService.shutdown();
+        return ReturnT.SUCCESS;
+    }
+}
