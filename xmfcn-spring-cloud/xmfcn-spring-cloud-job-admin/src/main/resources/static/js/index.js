ff6b2a8f73e2efd26ff6548112be6ade415d818b
airufei <zwjf2016>
2019-06-05 15:14:23 +0800

    封装时间控件

--- xmfcn-spring-cloud/xmfcn-spring-cloud-job-admin/src/main/resources/static/js/index.js
+++ xmfcn-spring-cloud/xmfcn-spring-cloud-job-admin/src/main/resources/static/js/index.js
@@ -3,42 +3,11 @@
  */
 $(function () {
 
-    // filter Time
-    var rangesConf = {};
-    rangesConf[I18n.daterangepicker_ranges_today] = [moment().startOf('day'), moment().endOf('day')];
-    rangesConf[I18n.daterangepicker_ranges_yesterday] = [moment().subtract(1, 'days').startOf('day'), moment().subtract(1, 'days').endOf('day')];
-    rangesConf[I18n.daterangepicker_ranges_this_month] = [moment().startOf('month'), moment().endOf('month')];
-    rangesConf[I18n.daterangepicker_ranges_last_month] = [moment().subtract(1, 'months').startOf('month'), moment().subtract(1, 'months').endOf('month')];
-    rangesConf[I18n.daterangepicker_ranges_recent_week] = [moment().subtract(1, 'weeks').startOf('day'), moment().endOf('day')];
-    rangesConf[I18n.daterangepicker_ranges_recent_month] = [moment().subtract(1, 'months').startOf('day'), moment().endOf('day')];
-
-    $('#filterTime').daterangepicker({
-        autoApply:false,
-        singleDatePicker:false,
-        showDropdowns:false,        // 是否显示年月选择条件
-        timePicker: true, 			// 是否显示小时和分钟选择条件
-        timePickerIncrement: 10, 	// 时间的增量，单位为分钟
-        timePicker24Hour : true,
-        opens : 'left', //日期选择框的弹出位置
-        ranges: rangesConf,
-        locale : {
-            format: 'YYYY-MM-DD HH:mm:ss',
-            separator : ' - ',
-            customRangeLabel : I18n.daterangepicker_custom_name ,
-            applyLabel : I18n.system_ok ,
-            cancelLabel : I18n.system_cancel ,
-            fromLabel : I18n.daterangepicker_custom_starttime ,
-            toLabel : I18n.daterangepicker_custom_endtime ,
-            daysOfWeek : I18n.daterangepicker_custom_daysofweek.split(',') ,        // '日', '一', '二', '三', '四', '五', '六'
-            monthNames : I18n.daterangepicker_custom_monthnames.split(',') ,        // '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'
-            firstDay : 1
-        },
-        startDate: rangesConf[I18n.daterangepicker_ranges_recent_month][0] ,
-        endDate: rangesConf[I18n.daterangepicker_ranges_recent_month][1]
-    }, function (start, end, label) {
-        freshChartDate(start, end);
-    });
-    freshChartDate(rangesConf[I18n.daterangepicker_ranges_recent_month][0], rangesConf[I18n.daterangepicker_ranges_recent_month][1]);
+    var  format="YYYY-MM-DD HH:mm:ss";
+    InitDateControl(format,freshChartDate);//初始化时间控件
+    var startTime=rangesConf[I18n.daterangepicker_ranges_recent_month][0];
+    var endTime=rangesConf[I18n.daterangepicker_ranges_recent_month][1];
+    freshChartDate(startTime,endTime);//初始化报表数据
 
     /**
      * fresh Chart Date
--- xmfcn-spring-cloud/xmfcn-spring-cloud-job-admin/src/main/resources/static/js/joblog.index.1.js
+++ xmfcn-spring-cloud/xmfcn-spring-cloud-job-admin/src/main/resources/static/js/joblog.index.1.js
@@ -1,5 +1,8 @@
 $(function () {
 
+    var format = "YYYY-MM-DD HH:mm:ss";
+    InitDateControl(format, null);//初始化时间控件
+
     // jobGroup change, job list init and select
     $("#jobGroup").on("change", function () {
         var jobGroup = $(this).children('option:selected').val();
@@ -34,40 +37,6 @@ $(function () {
         $("#jobGroup").change();
     }
 
-    // filter Time
-    var rangesConf = {};
-    rangesConf[I18n.daterangepicker_ranges_recent_hour] = [moment().subtract(1, 'hours'), moment()];
-    rangesConf[I18n.daterangepicker_ranges_today] = [moment().startOf('day'), moment().endOf('day')];
-    rangesConf[I18n.daterangepicker_ranges_yesterday] = [moment().subtract(1, 'days').startOf('day'), moment().subtract(1, 'days').endOf('day')];
-    rangesConf[I18n.daterangepicker_ranges_this_month] = [moment().startOf('month'), moment().endOf('month')];
-    rangesConf[I18n.daterangepicker_ranges_last_month] = [moment().subtract(1, 'months').startOf('month'), moment().subtract(1, 'months').endOf('month')];
-    rangesConf[I18n.daterangepicker_ranges_recent_week] = [moment().subtract(1, 'weeks').startOf('day'), moment().endOf('day')];
-    rangesConf[I18n.daterangepicker_ranges_recent_month] = [moment().subtract(1, 'months').startOf('day'), moment().endOf('day')];
-
-    $('#filterTime').daterangepicker({
-        autoApply: false,
-        singleDatePicker: false,
-        showDropdowns: false,        // 是否显示年月选择条件
-        timePicker: true, 			// 是否显示小时和分钟选择条件
-        timePickerIncrement: 10, 	// 时间的增量，单位为分钟
-        timePicker24Hour: true,
-        opens: 'left', //日期选择框的弹出位置
-        ranges: rangesConf,
-        locale: {
-            format: 'YYYY-MM-DD HH:mm:ss',
-            separator: ' - ',
-            customRangeLabel: I18n.daterangepicker_custom_name,
-            applyLabel: I18n.system_ok,
-            cancelLabel: I18n.system_cancel,
-            fromLabel: I18n.daterangepicker_custom_starttime,
-            toLabel: I18n.daterangepicker_custom_endtime,
-            daysOfWeek: I18n.daterangepicker_custom_daysofweek.split(','),        // '日', '一', '二', '三', '四', '五', '六'
-            monthNames: I18n.daterangepicker_custom_monthnames.split(','),        // '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'
-            firstDay: 1
-        },
-        startDate: rangesConf[I18n.daterangepicker_ranges_today][0],
-        endDate: rangesConf[I18n.daterangepicker_ranges_today][1]
-    });
 
     // init date tables
     var logTable = $("#joblog_list").dataTable({
