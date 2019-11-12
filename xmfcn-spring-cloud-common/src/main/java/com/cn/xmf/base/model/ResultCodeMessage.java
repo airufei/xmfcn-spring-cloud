11fdda40942778ed51ab194d3d3673dcc76c19f4
airufei <zwjf2016>
2019-11-12 18:19:05 +0800

    增加内容安全审查功能、增加缓存功能

--- xmfcn-spring-cloud-common/src/main/java/com/cn/xmf/base/model/ResultCodeMessage.java
+++ xmfcn-spring-cloud-common/src/main/java/com/cn/xmf/base/model/ResultCodeMessage.java
@@ -10,10 +10,12 @@ public class ResultCodeMessage {
     public static int SUCCESS = 200;// 成功
     public static int NO_DATA = 202;//暂时无数据
     public static int PARMS_ERROR = 400;//参数错误
+    public static int UNAUTHORIZED = 403;//未授权
     public static int DATA_ERROR = 440;//数据错误
     public static int FAILURE = 500;// 失败
 
     public static String NO_LOGIN_MESSAGE = "未登录";// 未登录
+    public static String UNAUTHORIZED_MESSAGE = "访问未授权";// 未登录
     public static String SUCCESS_MESSAGE = "成功";// 成功
     public static String FAILURE_MESSAGE = "失败";// 失败
     public static String EXCEPTION_MESSAGE = "服务器繁忙，请稍后再试";// 服务器繁忙，请稍后再试
--- xmfcn-spring-cloud-zuul/src/main/java/com/cn/xmf/zuul/filter/TokenFilter.java
+++ xmfcn-spring-cloud-zuul/src/main/java/com/cn/xmf/zuul/filter/TokenFilter.java
@@ -1,10 +1,19 @@
 package com.cn.xmf.zuul.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.cn.xmf.base.model.ResultCodeMessage;
+import com.cn.xmf.base.model.RetData;
+import com.cn.xmf.util.ConstantUtil;
+import com.cn.xmf.util.StringUtil;
+import com.cn.xmf.zuul.common.SysCommonService;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,10 +26,12 @@ import javax.servlet.http.HttpServletResponse;
  * <p>Description: 授权过滤器</p>
  */
 @Component
+@SuppressWarnings("all")
 public class TokenFilter extends ZuulFilter {
 
     private Logger logger = LoggerFactory.getLogger(TokenFilter.class);
-
+    @Autowired
+    private SysCommonService sysCommonService;
 
     /**
      * 下游服务之前进行处理
@@ -49,6 +60,14 @@ public class TokenFilter extends ZuulFilter {
      */
     @Override
     public boolean shouldFilter() {
+        // 根据之前的过滤器结果处理，若前一个过滤器失败，则该过滤器直接跳过
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String requestURI = request.getRequestURI();
+        boolean shouldFilter = false;
+        if (requestURI.contains("/wechat/")) {
+            shouldFilter = true;
+        }
         return false;
     }
 
@@ -62,6 +81,20 @@ public class TokenFilter extends ZuulFilter {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
         HttpServletResponse response = requestContext.getResponse();
+        String sourceCode = request.getParameter("sourceCode");
+        String dbCode = sysCommonService.getDictValue(ConstantUtil.DICT_TYPE_BASE_CONFIG, "sourceCode");
+        if (StringUtil.isBlank(dbCode)||!dbCode.equals(sourceCode)) {
+            requestContext.setSendZuulResponse(false);// 对该请求不路由
+            requestContext.set("isSuccess", false);// 设值，让下一个Filter看到上一个Filter的状态
+            // 构建返回信息
+            RetData retData = new RetData();
+            retData.setCode(ResultCodeMessage.NO_DATA);
+            retData.setMessage(ResultCodeMessage.UNAUTHORIZED_MESSAGE);
+            String jsonString = JSON.toJSONString(retData, SerializerFeature.WriteMapNullValue);
+            requestContext.setResponseBody(jsonString);
+            requestContext.addZuulResponseHeader("content-type", MediaType.APPLICATION_JSON_UTF8_VALUE);
+            return response;
+        }
         requestContext.set("isSuccess", true);// 设值，让下一个Filter看到上一个Filter的状态
         return null;
     }
